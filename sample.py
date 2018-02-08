7661f554c5488002bf806c38d33409b8658129f8
pholec <pholec@housebirnbaum.mit.edu>
2018-02-08 17:32:23 -0500

    sample scripts

---
+++ sample.py
@@ -0,0 +1,18 @@
+
+def new_sample():
+    return {
+            'contents':[],
+            'traceback':[],
+            'name':''
+            }
+    
+class DNA:
+
+    def __init__(self):
+        self.sequence = ''
+
+class Cell:
+
+    def 
+
+
